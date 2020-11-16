e398d32a7d927e98e11267b1ec244f7ed895c1e8
老齐 <qiwsir@gmail.com>
2020-11-16 08:42:09 +0800

    Update roman.py

--- roman.py
+++ roman.py
@@ -9,7 +9,7 @@ class Roman:
         convert={1000:'M',900:'CM',500:'D',400:'CD',100:'C',90:'XC',50:'L',40:'XL',10:'X',9:'IX',5:'V',4:'IV',1:'I'}
         result = []
         for i in convert:     #将想要转换的数字从列表中最大的开始比到最小的
-            while self.num >= i:
+            while self.num >= i:    ## 避免使用for里面嵌套while的写法。
                 result.append(convert[i])
                 self.num -= i
         return result   
